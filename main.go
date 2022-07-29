f519a74b703377fcd75bb42a6721ef07f4d7af25
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-07-29 13:15:40 -0700

    Add start to port checker tool

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cakturk/go-netstat/netstat"
+)
+
+func getOpenTCP6Ports() []netstat.SockTabEntry {
+	tabs6, err := netstat.TCP6Socks(func(s *netstat.SockTabEntry) bool {
+		return s.State == netstat.Listen
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tabs, err := netstat.TCPSocks(func(s *netstat.SockTabEntry) bool {
+		return s.State == netstat.Listen
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var tcpall [] netstat.SockTabEntry
+	tcpall = append(tcpall, tabs6[:]...)
+	tcpall = append(tcpall, tabs[:]...)
+	return tcpall
+}
+
+func main() {
+	tcp6Ports := getOpenTCP6Ports()
+
+	for _, p := range tcp6Ports {
+		fmt.Printf("%10v %10v", p.LocalAddr.IP,  p.LocalAddr.Port)
+		if p.Process != nil {
+			fmt.Printf("%10v %10v\n", p.Process.Name, p.Process.Pid)
+		} else {
+			fmt.Printf("%10v %10v\n", "no name", "no pid")
+		}
+	}
+}
