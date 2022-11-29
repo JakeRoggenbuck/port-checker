c651e7d1531974135918b1646e87e564228fab00
Jake <jakeroggenbuck2@gmail.com>
2022-11-28 23:54:03 -0800

    Add watch

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/cakturk/go-netstat/netstat"
+	"time"
 )
 
 func getOpenTCP6Ports() []netstat.SockTabEntry {
@@ -20,23 +22,45 @@ func getOpenTCP6Ports() []netstat.SockTabEntry {
 		fmt.Println(err)
 	}
 
-	var tcpall [] netstat.SockTabEntry
+	var tcpall []netstat.SockTabEntry
 	tcpall = append(tcpall, tabs6[:]...)
 	tcpall = append(tcpall, tabs[:]...)
 	return tcpall
 }
 
-func main() {
+func single_run() {
 	tcp6Ports := getOpenTCP6Ports()
 
 	fmt.Printf("%12v %12v %12v %12v\n", "Local IP", "Local Port", "Proc Name", "Proc ID")
 	fmt.Printf("%12v %12v %12v %12v\n", "--------", "----------", "---------", "-------")
 	for _, p := range tcp6Ports {
-		fmt.Printf("%12v %12v", p.LocalAddr.IP,  p.LocalAddr.Port)
+		fmt.Printf("%12v %12v", p.LocalAddr.IP, p.LocalAddr.Port)
 		if p.Process != nil {
 			fmt.Printf(" %12v %12v\n", p.Process.Name, p.Process.Pid)
 		} else {
 			fmt.Printf(" %12v %12v\n", " --- ", " --- ")
 		}
+	}
+}
+
+func clear_screen() {
+	fmt.Println("\033[2J")
+}
+
+func main() {
+	var seconds time.Duration = 2
+
+	var watch bool
+	flag.BoolVar(&watch, "watch", false, "update view in watch mode")
+	flag.Parse()
+
+	if watch {
+		for {
+			single_run()
+			time.Sleep(seconds * time.Second)
+			clear_screen()
+		}
+	} else {
+		single_run()
 	}
 }
